d4de58320e9545f44fdddb10f6ba64facf607afc
Aiden McLachlan <MCLACHAT@mclachat>
2016-05-22 11:09:39 +1000

    updated java
    Started programing the JS including the calculations. still doesn't do
    anything to the HTML and won't display the output yet.

--- Java-Doc.js
+++ Java-Doc.js
@@ -1,1 +1,22 @@
+function income() {
+   var inputNameElement = document.getElementById("inputContent");
+   var I = inputNameElement.value;
+   var titleElement = document.getElementById("title");
 
+  
+if (I<16000){
+   titleElement.innerHTML = "taxpayed = $0";
+}
+else if (I<52500){
+  titleElement.innerHTML = ("tax payed = $" + ((R - 16000) * 0.1));
+}
+else if (I<113000){
+  titleElement.innerHTML = ("tax payed = $" + (((R - 52500)* 0.16) + 3650));
+}
+else if (I<184500){
+  titleElement.innerHTML = ("tax payed = $" + (((R - 113000)* 0.24) + 13330));
+}
+else{
+  titleElement.innerHTML = ("tax payed = $" + (((R - 184500)* 0.36) + 30490));
+}
+}
