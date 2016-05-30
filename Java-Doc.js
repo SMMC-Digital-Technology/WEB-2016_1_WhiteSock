e762b0889be68c2ffa17d1b8360fb4d1f0442031
Aiden McLachlan <MCLACHAT@mclachat>
2016-05-30 15:11:25 +1000

    updated JS
    Updated the JS to display the text however won't display the number at
    the moment

--- Java-Doc.js
+++ Java-Doc.js
@@ -1,23 +1,28 @@
 function income() {
    var inputNameElement = document.getElementById("inputContent");
    var I = inputNameElement.value;
-   var innerHTML = document.getElementById("outputContent");
-
+   var outputContent = document.getElementById("outputContent");
+   var R = inputContent.value;
   
-if (I<16000){
-   var TP = "taxpayed = $0";
-   statementDisplay.innerHTML = "tax payed = $" + TP;
+  
+if (I < 16000){
+   var TP = 0;
+   outputContent.innerHTML = "tax payed = $" + TP;
 }
 else if (I<52500){
   var TP = (R - 16000) * 0.1;
+    outputContent.innerHTML = "tax payed = $" + TP;
 }
 else if (I<113000){
   var TP = (((R - 52500)* 0.16) + 3650);
+  outputContent.innerHTML = "tax payed = $" + TP;
 }
 else if (I<184500){
   var TP = (((R - 113000)* 0.24) + 13330);
+ outputContent.innerHTML = "tax payed = $" + TP;
 }
 else{
   var TP = (((R - 184500)* 0.36) + 30490);
+  outputContent.innerHTML = "tax payed = $" + TP;
 }
 }
