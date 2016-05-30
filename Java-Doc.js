d26f472d93f090019ae4f664af72e6411e6920af
Aiden McLachlan <MCLACHAT@mclachat>
2016-05-31 09:11:17 +1000

    The JS is working
    Finished the JS for the input and out put the Graph still needs to be
    done

--- Java-Doc.js
+++ Java-Doc.js
@@ -1,28 +1,28 @@
 function income() {
-   var inputNameElement = document.getElementById("inputContent");
+   var inputNameElement = document.getElementById("income");
    var I = inputNameElement.value;
    var outputContent = document.getElementById("outputContent");
-   var R = inputContent.value;
-  
+   var R = inputNameElement.value;
+  var TP = 0;
   
 if (I < 16000){
-   var TP = 0;
+   TP = 0;
    outputContent.innerHTML = "tax payed = $" + TP;
 }
-else if (I<52500){
-  var TP = (R - 16000) * 0.1;
+else if (I < 52500){
+  TP = (R - 16000) * 0.1;
     outputContent.innerHTML = "tax payed = $" + TP;
 }
-else if (I<113000){
-  var TP = (((R - 52500)* 0.16) + 3650);
+else if (I < 113000){
+  TP = (((R - 52500)* 0.16) + 3650);
   outputContent.innerHTML = "tax payed = $" + TP;
 }
-else if (I<184500){
-  var TP = (((R - 113000)* 0.24) + 13330);
+else if (I < 184500){
+  TP = (((R - 113000)* 0.24) + 13330);
  outputContent.innerHTML = "tax payed = $" + TP;
 }
 else{
-  var TP = (((R - 184500)* 0.36) + 30490);
+  TP = (((R - 184500)* 0.36) + 30490);
   outputContent.innerHTML = "tax payed = $" + TP;
 }
 }
