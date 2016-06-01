8661c5cbc18bf32c1b1c6b0f092ee1f555775f50
Aiden McLachlan <MCLACHAT@mclachat>
2016-06-01 20:42:06 +1000

    finished mostly everything
    all that is left to finish is the spelling/grammar check and the
    refernceing of the photo

--- Java-Doc.js
+++ Java-Doc.js
@@ -1,28 +1,27 @@
 function income() {
-   var inputNameElement = document.getElementById("income");
-   var I = inputNameElement.value;
-   var outputContent = document.getElementById("outputContent");
-   var R = inputNameElement.value;
+   var inputNameElement = document.getElementById("income"); /* to read the income box */
+   var I = inputNameElement.value; /* to set the value of Income to var 'I' */
+   var outputContent = document.getElementById("outputContent"); /* sets the text so can change */
   var TP = 0;
   
-if (I < 16000){
+if (I < 16000){ /* the first income bracket */
    TP = 0;
-   outputContent.innerHTML = "tax payed = $" + TP;
+   outputContent.innerHTML = "Tax Paid = $" + TP;
 }
-else if (I < 52500){
-  TP = (R - 16000) * 0.1;
-    outputContent.innerHTML = "tax payed = $" + TP;
+else if (I < 52500){ /* the seccond income bracket */
+  TP = (I - 16000) * 0.1;
+    outputContent.innerHTML = "Tax Paid = $" + TP;
 }
-else if (I < 113000){
-  TP = (((R - 52500)* 0.16) + 3650);
-  outputContent.innerHTML = "tax payed = $" + TP;
+else if (I < 113000){ /* the third income bracket */
+  TP = (((I - 52500)* 0.16) + 3650);
+  outputContent.innerHTML = "Tax Paid = $" + TP;
 }
-else if (I < 184500){
-  TP = (((R - 113000)* 0.24) + 13330);
- outputContent.innerHTML = "tax payed = $" + TP;
+else if (I < 184500){ /* the fourth income bracket */
+  TP = (((I - 113000)* 0.24) + 13330);
+ outputContent.innerHTML = "Tax Paid = $" + TP;
 }
-else{
-  TP = (((R - 184500)* 0.36) + 30490);
-  outputContent.innerHTML = "tax payed = $" + TP;
+else{ /* the fifth income bracket */
+  TP = (((I - 184500)* 0.36) + 30490);
+  outputContent.innerHTML = "Tax Paid = $" + TP;
 }
 }
